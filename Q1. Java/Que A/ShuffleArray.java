e62be08c13d2568b692194e1ca4eef047b24c050
Aryan Barnwal <77083847+aryanbarnwal01@users.noreply.github.com>
2023-10-10 12:57:42 +0530

    Update ShuffleArray.java
    Removing the comment lines

--- Q1. Java/Que A/ShuffleArray.java
+++ Q1. Java/Que A/ShuffleArray.java
@@ -1,7 +1,6 @@
 public class ShuffleArray {
     public static void main(String[] args) {
         int[] array = { 1, 2, 3, 4, 5, 6, 7 };
-        // Function to Shuffle Array by taking parameter as array
         shuffleArray(array);
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
@@ -11,9 +10,7 @@ public class ShuffleArray {
     public static void shuffleArray(int[] array) {
         int n = array.length;
         for (int i = n - 1; i >= 0; i--) {
-            // Random index is chosen between the index(5,1)
             int j = (int) (Math.random() * (i + 1));
-            // Swaping is happening between random number between the index(0,6)
             int temp = array[i];
             array[i] = array[j];
             array[j] = temp;
